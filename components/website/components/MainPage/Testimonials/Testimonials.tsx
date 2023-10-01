34925429a85cad0f689990f140c818fb920e4a75
rustem <dzhuraievrustem@gmail.com>
2023-10-01 05:53:45 +0200

    swipe js for testimonials

--- components/website/components/MainPage/Testimonials/Testimonials.tsx
+++ components/website/components/MainPage/Testimonials/Testimonials.tsx
@@ -1,20 +1,12 @@
 "use client";
 import React from "react";
-import Slider from "react-slick";
 import { IconContext } from "react-icons";
 import { BiSolidQuoteLeft } from "react-icons/bi";
-
-const settings = {
-	dots: true,
-	infinite: true,
-	speed: 500,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	autoplay: true,
-	autoplaySpeed: 3000,
-	fade: true,
-	swipeToSlide: true,
-};
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Mousewheel, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 export default function Testimonials() {
 	const feedbacks = [
@@ -30,33 +22,65 @@ export default function Testimonials() {
 			text: "Beautiful piece of furniture. A true asset to my audio and video system. We spend a lot of time finding just the right TV stand for the intended space, and it looks like we finally found it. i was a little afraid the high gloss might look tacky, but the clean lines just look understated, unobtrusive and quite beautiful.",
 			author: "Andrey L.",
 		},
+		{
+			text: "Beautiful piece of furniture. A true asset to my audio and video system. We spend a lot of time finding just the right TV stand for the intended space, and it looks like we finally found it. i was a little afraid the high gloss might look tacky, but the clean lines just look understated, unobtrusive and quite beautiful.",
+			author: "Andrey L.",
+		},
 	];
 	return (
 		<div>
 			<h2 className="text-24 font-bold text-testimonials text-center tracking-[0.48px] mb-30">
 				Testimonials
 			</h2>
-			<Slider {...settings}>
+			<Swiper
+				direction={"horizontal"}
+				slidesPerView={1}
+				centeredSlides={true}
+				spaceBetween={20}
+				grabCursor={true}
+				breakpoints={{
+					768: {
+						slidesPerView: 3,
+						spaceBetween: 20,
+						centeredSlides: false,
+					},
+					1440: {
+						slidesPerView: 3,
+						spaceBetween: 20,
+						centeredSlides: false,
+					},
+				}}
+				mousewheel={{
+					forceToAxis: true, // Enable forceToAxis to scroll only horizontally
+					releaseOnEdges: true, // Allow releasing on edges to continue page scroll
+				}}
+				pagination={{
+					dynamicBullets: true,
+					clickable: true,
+				}}
+				modules={[Mousewheel, Pagination]}>
 				{feedbacks.map((item, index) => (
-					<div
-						key={index}
-						className="px-20 py-30 border-testimonials rounded-24">
-						<IconContext.Provider value={{ size: "40px", color: "#FF008A" }}>
-							<div className="flex justify-start mb-20">
-								<BiSolidQuoteLeft />
+					<SwiperSlide key={index}>
+						<div
+							key={index}
+							className="px-20 py-30 border-testimonials rounded-24 h-400 overflow-y-scroll scrollbar-hidden">
+							<IconContext.Provider value={{ size: "40px", color: "#FF008A" }}>
+								<div className="flex justify-start mb-20">
+									<BiSolidQuoteLeft />
+								</div>
+							</IconContext.Provider>
+							<div>
+								<p className="text-testimonials text-16 leading-[1.7] tracking-[0.32px] mb-16">
+									{item.text}
+								</p>
+								<p className="text-testimonials font-medium text-20 leading-[1.5] tracking-[0.4px]">
+									{item.author}
+								</p>
 							</div>
-						</IconContext.Provider>
-						<div>
-							<p className="text-testimonials text-16 leading-[1.7] tracking-[0.32px] mb-16">
-								{item.text}
-							</p>
-							<p className="text-testimonials font-medium text-20 leading-[1.5] tracking-[0.4px]">
-								{item.author}
-							</p>
 						</div>
-					</div>
+					</SwiperSlide>
 				))}
-			</Slider>
+			</Swiper>
 		</div>
 	);
 }
