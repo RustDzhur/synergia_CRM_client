02f12fdb9c2d3fe2f2de4a539b08864dea3571a0
rustem <dzhuraievrustem@gmail.com>
2023-10-10 00:21:38 +0200

    bugs fixig with login

--- app/store/useAuthStore.ts
+++ app/store/useAuthStore.ts
@@ -1,95 +1,98 @@
 import { create } from "zustand";
 
 interface SignUpFormData {
-	firstname: string;
-	lasttname: string;
-	email: string;
-	password: string;
+  firstname: string;
+  lasttname: string;
+  email: string;
+  password: string;
 }
 
 interface SignInFormData {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 }
 
 interface AuthStore {
-	isSigningIn: boolean;
-	isSigningUp: boolean;
-	timeoutId?: NodeJS.Timeout;
-	signIn: (data: SignInFormData) => Promise<void>;
-	signUp: (data: SignUpFormData) => Promise<void>;
+  isSigningIn: boolean;
+  isSigningUp: boolean;
+  timeoutId?: NodeJS.Timeout;
+  signIn: (data: SignInFormData) => Promise<void>;
+  signUp: (data: SignUpFormData) => Promise<void>;
 }
 
-const useAuthStore = create<AuthStore>((set) => ({
-	isSigningIn: false,
-	isSigningUp: false,
-	signIn: async (data: SignInFormData) => {
-		// Set loading state
-		set({ isSigningIn: true });
+const useAuthStore = create<AuthStore>((set) => {
+  const tokenFromLocalStorage = localStorage.getItem("token");
+  const initialIsSigningIn = tokenFromLocalStorage !== null && tokenFromLocalStorage !== undefined;
+  
+  set({
+    isSigningIn: initialIsSigningIn,
+    isSigningUp: false,
+    timeoutId: undefined,
+  });
 
-		try {
-			// Make API call for signin
-			const response = await fetch(
-				"https://synergia-crm-server.onrender.com/api/signin",
-				{
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify(data),
-				}
-			);
+  return {
+    isSigningIn: initialIsSigningIn,
+    isSigningUp: false,
+    timeoutId: undefined,
+    signIn: async (data: SignInFormData) => {
+      set({ isSigningIn: true });
 
-			if (!response.ok) {
-				throw new Error("Signin failed");
-			}
+      try {
+        const response = await fetch(
+          "https://synergia-crm-server.onrender.com/api/signin",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          }
+        );
 
-			const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error("Signin failed");
+        }
 
-			localStorage.setItem("token", responseData.user.token);
+        const responseData = await response.json();
 
-			const timeoutId = setTimeout(() => {
-				localStorage.setItem("token", "");
-				set({ isSigningIn: false });
-			}, 23 * 60 * 60 * 1000 + 59 * 60 * 1000 + 59 * 1000);
+        localStorage.setItem("token", responseData.user.token);
 
-			set({ timeoutId });
-		} catch (error) {
-			console.error("Signin error:", error);
-		}
-	},
-	signUp: async (data: SignUpFormData) => {
-		// Set loading state
-		set({ isSigningUp: true });
+        const timeoutId = setTimeout(() => {
+          localStorage.setItem("token", "");
+          set({ isSigningIn: false });
+        }, 23 * 60 * 60 * 1000 + 59 * 60 * 1000 + 59 * 1000);
 
-		try {
-			// Make API call for signup
-			const response = await fetch(
-				"https://synergia-crm-server.onrender.com/api/signup",
-				{
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify(data),
-				}
-			);
+        set({ timeoutId });
+      } catch (error) {
+        console.error("Signin error:", error);
+      }
+    },
+    signUp: async (data: SignUpFormData) => {
+      set({ isSigningUp: true });
 
-			// Handle response
-			if (!response.ok) {
-				throw new Error("Signup failed");
-			}
+      try {
+        const response = await fetch(
+          "https://synergia-crm-server.onrender.com/api/signup",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          }
+        );
 
-			// Handle successful signup, e.g., set user token in local storage, etc.
-			// ...
-		} catch (error) {
-			// Handle error
-			console.error("Signup error:", error);
-		} finally {
-			// Reset loading state
-			set({ isSigningUp: false });
-		}
-	},
-}));
+        if (!response.ok) {
+          throw new Error("Signup failed");
+        }
+
+      } catch (error) {
+        console.error("Signup error:", error);
+      } finally {
+        set({ isSigningUp: false });
+      }
+    },
+  };
+});
 
 export default useAuthStore;
