a15ba1afa287549cee83eecfb0e79c9ef8156988
RustDzhur <dzhuraievrustem@gmail.com>
2026-06-29 22:50:18 +0200

    kleine changes

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export default createMiddleware({
   locales: ['ua', 'en', 'de'],
  
   // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-  defaultLocale: 'ua'
+  defaultLocale: 'de'
 });
  
 export const config = {
